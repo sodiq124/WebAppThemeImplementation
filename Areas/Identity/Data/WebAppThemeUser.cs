63d969aa6c190aefa72df67e268bc3d275ba8e80
Olakunmi <agboolaolakunmi@gmail.com>
2020-11-20 15:19:41 +0100

    Add project files.

---
+++ Areas/Identity/Data/WebAppThemeUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebAppThemeImplementation.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the WebAppThemeUser class
+    public class WebAppThemeUser : IdentityUser
+    {
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using WebAppThemeImplementation.Areas.Identity.Data;
+//using WebAppThemeImplementation.Data;
+
+//[assembly: HostingStartup(typeof(WebAppThemeImplementation.Areas.Identity.IdentityHostingStartup))]
+//namespace WebAppThemeImplementation.Areas.Identity
+//{
+//    public class IdentityHostingStartup : IHostingStartup
+//    {
+//        public void Configure(IWebHostBuilder builder)
+//        {
+//            builder.ConfigureServices((context, services) => {
+//                services.AddDbContext<WebDBContext>(options =>
+//                    options.UseSqlServer(
+//                        context.Configuration.GetConnectionString("WebDBContextConnection")));
+
+//                services.AddDefaultIdentity<WebAppThemeUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//                    .AddEntityFrameworkStores<WebDBContext>();
+//            });
+//        }
+//    }
+//}
